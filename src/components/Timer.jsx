e36d968da06af6a315cfafb4b1d8c3a30f34db87
Kevin Santamaria <kevinsdasere@gmail.com>
2025-07-20 10:46:22 -0400

    Fixing the timmer sessions, implementing the reset button and the 'add' buttons

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -5,14 +5,14 @@ function Timer() {
   const [time, setTime] = useState(25 * 60);
   const [isActive, setIsActive] = useState(false);
   const [typeOfSession, setTypeOfSession] = useState("focus"); // or 'shortBreak', or 'longBreak'
-  const [numOfSession, setNumOfSession] = useState(1);
+  const [numOfSession, setNumOfSession] = useState(0);
 
   useEffect(() => {
     let timer;
 
     if (isActive) {
       timer = setInterval(() => {
-        setTime((prev) => {
+        setTime((prev) => { // decrement the time every second
           if (prev === 1) {
             handleTimerEnd();
             return 0;
@@ -27,23 +27,38 @@ function Timer() {
   const handleTimerEnd = () => {
     setIsActive(false);
     if (typeOfSession === "focus") {
-      setTypeOfSession("shortBreak");
-      setTime(5 * 60);
-    } else if (typeOfSession === "focus" && numOfSession === 4) {
-      setTypeOfSession("longBreak");
-      setTime(15 * 60);
-    }
-    else {
+      const nextSession = numOfSession + 1;
+      if (nextSession % 4 === 0) {
+        setTypeOfSession("longBreak");
+        setTime(15 * 60);
+      } else {
+        setTypeOfSession("shortBreak");
+        setTime(5 * 60);
+      }
+      setNumOfSession(nextSession);
+    } else {
       setTypeOfSession("focus");
-      setTime(25*60)
+      setTime(25 * 60);
     }
+  };
+
+  const handleReset = () => {
+    setIsActive(false);
+    setTime(
+      typeOfSession === "focus"
+        ? 25 * 60
+        : typeOfSession === "shortBreak"
+        ? 5 * 60
+        : 15 * 60
+    );
   };
 
   return (
     <section className="bg-white h-screen flex flex-col gap-5 items-center justify-center">
       <article className="fixed top-5 right-5 bg-red-800 rounded-xl p-2">
         <h2 className="font-bold text-white text-xl">
-          Current Session: <span className="font-normal">{numOfSession}</span>
+          Sessions Completed:{" "}
+          <span className="font-normal">{numOfSession}</span>
         </h2>
       </article>
       <article className="flex gap-6">
@@ -63,10 +78,30 @@ function Timer() {
         </span>
       </article>
       <article className="flex gap-3">
-        <span className="border p-1.5 rounded-xl cursor-pointer">+ 25 min</span>
-        <span className="border p-1.5 rounded-xl cursor-pointer">+ 10 min</span>
-        <span className="border p-1.5 rounded-xl cursor-pointer">+ 5 min</span>
-        <span className="border p-1.5 rounded-xl cursor-pointer">+ 1 min</span>
+        <span
+          className="border p-1.5 rounded-xl cursor-pointer"
+          onClick={() => setTime(time + 25 * 60)}
+        >
+          + 25 min
+        </span>
+        <span
+          className="border p-1.5 rounded-xl cursor-pointer"
+          onClick={() => setTime(time + 10 * 60)}
+        >
+          + 10 min
+        </span>
+        <span
+          className="border p-1.5 rounded-xl cursor-pointer"
+          onClick={() => setTime(time + 5 * 60)}
+        >
+          + 5 min
+        </span>
+        <span
+          className="border p-1.5 rounded-xl cursor-pointer"
+          onClick={() => setTime(time + 1 * 60)}
+        >
+          + 1 min
+        </span>
       </article>
       <article className="flex gap-4">
         <button
@@ -75,13 +110,12 @@ function Timer() {
             setIsActive(!isActive);
           }}
         >
-          {isActive ? <CirclePlay /> : <CirclePause />}
+          {isActive ? <CirclePause /> : <CirclePlay />}
+          <span>{isActive ? "Pause" : "Play"}</span>
         </button>
         <button
           className="flex gap-1 bg-red-800 p-2.5 rounded-xl text-white cursor-pointer font-bold"
-          onClick={() => {
-            setIsActive(false);
-          }}
+          onClick={handleReset}
         >
           <TimerReset />
           Reset
