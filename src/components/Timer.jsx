79a438648caa0a086e43f25040faf6102bfca1a6
Kevin Santamaria <kevinsdasere@gmail.com>
2025-07-19 21:02:14 -0400

    Implementing the timer with an interval and the 'handle timer end' function

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,12 +1,43 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { CirclePlay, TimerReset, CirclePause } from "lucide-react";
 
-function Timer({ initialTime, numOfSession = 0 }) {
-  const [time, setTime] = useState(initialTime);
-  const [active, setActive] = useState(false);
-  const [session, setSession] = useState(numOfSession);
+function Timer() {
+  const [time, setTime] = useState(25 * 60);
+  const [isActive, setIsActive] = useState(false);
+  const [typeOfSession, setTypeOfSession] = useState("focus"); // or 'shortBreak', or 'longBreak'
+  const [numOfSession, setNumOfSession] = useState(1);
 
-  console.log(active);
+  useEffect(() => {
+    let timer;
+
+    if (isActive) {
+      timer = setInterval(() => {
+        setTime((prev) => {
+          if (prev === 1) {
+            handleTimerEnd();
+            return 0;
+          }
+          return prev - 1;
+        });
+      }, 1000);
+    }
+    return () => clearInterval(timer);
+  }, [isActive]);
+
+  const handleTimerEnd = () => {
+    setIsActive(false);
+    if (typeOfSession === "focus") {
+      setTypeOfSession("shortBreak");
+      setTime(5 * 60);
+    } else if (typeOfSession === "focus" && numOfSession === 4) {
+      setTypeOfSession("longBreak");
+      setTime(15 * 60);
+    }
+    else {
+      setTypeOfSession("focus");
+      setTime(25*60)
+    }
+  };
 
   return (
     <section className="bg-white h-screen flex flex-col gap-5 items-center justify-center">
@@ -27,7 +58,9 @@ function Timer({ initialTime, numOfSession = 0 }) {
         </button>
       </article>
       <article className="border-b-gray-700 m-5">
-        <span className="text-7xl">{initialTime}</span>
+        <span className="text-7xl">
+          {Math.floor(time / 60)}:{String(time % 60).padStart(2, "00")}
+        </span>
       </article>
       <article className="flex gap-3">
         <span className="border p-1.5 rounded-xl cursor-pointer">+ 25 min</span>
@@ -35,24 +68,19 @@ function Timer({ initialTime, numOfSession = 0 }) {
         <span className="border p-1.5 rounded-xl cursor-pointer">+ 5 min</span>
         <span className="border p-1.5 rounded-xl cursor-pointer">+ 1 min</span>
       </article>
-      <button
-        className="flex gap-1 bg-red-800 p-2.5 rounded-xl text-white cursor-pointer font-bold"
-        onClick={() => {
-          setActive(true);
-        }}
-      >
-        <CirclePlay />
-        Start
-      </button>
       <article className="flex gap-4">
-        <button className="flex gap-1 bg-red-800 p-2.5 rounded-xl text-white cursor-pointer font-bold">
-          <CirclePause />
-          Pause
+        <button
+          className="flex gap-1 bg-red-800 p-2.5 rounded-xl text-white cursor-pointer font-bold"
+          onClick={() => {
+            setIsActive(!isActive);
+          }}
+        >
+          {isActive ? <CirclePlay /> : <CirclePause />}
         </button>
         <button
           className="flex gap-1 bg-red-800 p-2.5 rounded-xl text-white cursor-pointer font-bold"
           onClick={() => {
-            setActive(false);
+            setIsActive(false);
           }}
         >
           <TimerReset />
@@ -62,5 +90,4 @@ function Timer({ initialTime, numOfSession = 0 }) {
     </section>
   );
 }
-
 export default Timer;
